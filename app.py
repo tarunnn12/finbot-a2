18d6e292b818b238f0cf84418ae7c070a6014354
tarunnn12 <verma.tarunggn283@gmail.com>
2026-04-13 02:41:35 +1000

    fix indentation in app.py

--- app.py
+++ app.py
@@ -166,4 +166,4 @@ def reset():
 # ============================================================
 if __name__ == "__main__":
      port = int(os.environ.get("PORT", 5000))
-    app.run(debug=False, host="0.0.0.0", port=port)
+     app.run(debug=False, host="0.0.0.0", port=port)
